d830a1f152d21e4f10a9d468cf1460657adaeab9
Seth Vargas <shvargas01@gmail.com>
2023-02-20 07:54:06 -0600

    Updated the app to use Flask Sessions, along with some minor tweaks

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, flash, redirect, jsonify
+from flask import session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from surveys import satisfaction_survey
@@ -9,7 +10,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False # makes it so that the debugg
 
 debug = DebugToolbarExtension(app)
 
-responses = [] # As people answer questions, you should store answers in this list. 
+# responses = [] # As people answer questions, you should store answers in this list. 
 """
 NOTE
 responses does not reset until the server stops. You may need to stop the server if you run into any bugs, as the legnth of this list tells the app to end once it exceeds the length of avaliable questions.
@@ -22,16 +23,26 @@ def show_survey():
     return render_template("index.html", title=title, instructions=instructions)
 
 
+@app.route('/start-test', methods=["POST"])
+def start_test():
+    # cannot submit multiple forms
+    if not session['responses']:
+        session['responses'] = []
+        return redirect('questions/0')
+    flash("You have already completed the test, thank you!")
+    return redirect('/complete')
+
+
 @app.route('/questions/<int:question_number>')
 def show_question(question_number):
     current_question = satisfaction_survey.questions[question_number].question
     choices = [c for c in satisfaction_survey.questions[question_number].choices]
 
-    if question_number > len(responses):
+    if question_number > len(session['responses']):
         flash("Invalid number entered in URL")
-        return redirect(f'/questions/{len(responses)}')
+        return redirect(f"/questions/{len(session['responses'])}")
 
-    if len(satisfaction_survey.questions) == len(responses):
+    if len(satisfaction_survey.questions) == len(session['responses']):
         return redirect('/complete')
 
     return render_template("questions.html", question=current_question, number=question_number, choices=choices)
@@ -41,12 +52,16 @@ def show_question(question_number):
 def store_answers():
     # append to responses list
     ans = request.form['choices']
-    responses.append(ans)
-    next_question = len(responses)
+
+    answers_list = session['responses']
+    answers_list.append(ans)
+    session['responses'] = answers_list
+
     # survey is over, thank the user
-    if next_question == len(satisfaction_survey.questions):
+    if len(session['responses']) == len(satisfaction_survey.questions):
         return redirect("/complete")
     # redirect them to the next question
+    next_question = len(session['responses'])
     return redirect(f'/questions/{next_question}')
 
 
